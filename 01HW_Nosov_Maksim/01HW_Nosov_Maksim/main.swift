ce44f6d785df211e21c46daad3a95c273cdf03ed
Maksim Nosov <ozolc@yandex.ru>
2018-11-23 20:02:44 +0300

    Добавлен код для первого задания.

--- 01HW_Nosov_Maksim/01HW_Nosov_Maksim/main.swift
+++ 01HW_Nosov_Maksim/01HW_Nosov_Maksim/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+// 1. Написать функцию, которая определяет, четное число или нет.
+
+func checkEven(_ num: Int) -> Bool {
+    if num % 2 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+let numberArray = [3, 5, 1, 0, 7, 10, 2]
+
+for i in 0...numberArray.count - 1 {
+    if checkEven(numberArray[i]) { print ("Число \(numberArray[i]) - четное")}
+    else { print ("Число \(numberArray[i]) - нечетное")}
+}
 
