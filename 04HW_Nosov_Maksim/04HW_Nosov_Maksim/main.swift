f6dbec81a53d43d1ca05b8cb6efbbbf09e5c58de
Maksim Nosov <ozolc@yandex.ru>
2018-11-30 19:38:18 +0300

    Первый коммит ДЗ №4

---
+++ 04HW_Nosov_Maksim/04HW_Nosov_Maksim/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  04HW_Nosov_Maksim
+//
+//  Created by Maksim Nosov on 30/11/2018.
+//  Copyright © 2018 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
