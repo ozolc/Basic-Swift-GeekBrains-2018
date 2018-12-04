b58607d6bcfbbe0c0a72e72f3635ee2a1a98269a
Maksim Nosov <ozolc@yandex.ru>
2018-12-04 14:47:05 +0300

    First commit for 05HW_Nosov_Maksim

---
+++ 05HW_Nosov_Maksim/05HW_Nosov_Maksim/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  05HW_Nosov_Maksim
+//
+//  Created by NosovML on 04/12/2018.
+//  Copyright © 2018 NosovML. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
