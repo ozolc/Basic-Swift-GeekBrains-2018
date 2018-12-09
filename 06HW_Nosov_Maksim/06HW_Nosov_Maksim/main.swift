e727551cd4f7a94ce62a27ff01e081917c42212b
Maksim Nosov <ozolc@yandex.ru>
2018-12-09 18:23:31 +0300

    Реализовано второе задание из ДЗ

--- 06HW_Nosov_Maksim/06HW_Nosov_Maksim/main.swift
+++ 06HW_Nosov_Maksim/06HW_Nosov_Maksim/main.swift
@@ -63,6 +63,20 @@ public struct Queue<T>: CustomStringConvertible {
     public var description: String {
         return "Содержимое очереди: \(self.array)"
     }
+    
+    public func filter(array: [T], predicateSomeClosure: (T) -> Bool) -> [T] {
+        
+        var tmpArray = [T]()
+        
+        for element in array {
+            if predicateSomeClosure(element) {
+                tmpArray.append(element)
+            }
+        }
+        
+        return tmpArray
+    }
+    
 }
 
 print("Реализация очереди с использованием Generics - (медленная реализация)")
@@ -71,14 +85,24 @@ print(q01)
 q01.enqueue("Swift")
 q01.enqueue("C++")
 q01.enqueue("Java")
-print(q01.getCount())
+print(q01.getCount(), "\n")
+print("\(q01)")
+
+let sortedQueue = q01.array.sorted(by: >)
+print("Отсортировання очередь по убыванию: \(sortedQueue)\n")
+
+let filteredQueue = q01.filter(array: q01.array) { (s1: String) -> Bool in
+    return s1 == "Swift"
+}
+print("Массив из очереди по фильтру: \(filteredQueue)\n")
 
 while (q01.count > 0) {
-q01.dequeue()
-print(q01)
-print(q01.getCount())
+    q01.dequeue()
+    print(q01)
+    print(q01.getCount())
 }
 print("=========")
+
 
 // MARK: 01_02. Queue - more efficient realization
 // Более эффективная реализация очереди.
